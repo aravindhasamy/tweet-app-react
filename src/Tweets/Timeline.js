09e145e799eb04a4bc1beda34d06a4063131b4a6
aravindhasamy <aravindhasamy9@gmail.com>
2021-09-04 14:09:38 +0530

    tweet update feature added

--- src/Tweets/Timeline.js
+++ src/Tweets/Timeline.js
@@ -1,60 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import classes from "./Tweet.module.css";
-import TweetItem from './TweetItem';
+import TweetItem from "./TweetItem";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Timeline(props) {
-    const [tweetList, setTweetList] = useState([]);
-    const [useEffectDependency, setUseUffectDependency] = useState([]);
-    useEffect(() => {
-        async function fetchData ()  {
-            let url = 'http://localhost:3005/tweets';
-        
-            let response = await fetch(url);
-    
-            let tweets = await response.json(); 
-            setTweetList(tweets)
-        }
-        fetchData();
-      }, [useEffectDependency]);
+  const [tweetList, setTweetList] = useState([]);
+  const [useEffectDependency, setUseUffectDependency] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      let url = "http://localhost:3005/tweets";
 
-      const removeTweetHandler = async (tweetItem) => {
-          setUseUffectDependency(tweetItem)
+      let response = await fetch(url);
 
-          let url = `http://localhost:3005/tweets/${tweetItem._id}`;
-        let response = await fetch(url, {
-            method: "DELETE",
-            headers: {
-              "Content-Type": "application/json;charset=utf-8",
-            }
-          });
-        let result = await response.json();
-          console.log("result", result)
-      }
-    
-        const Tweets = tweetList.map((tweet) => (
-            <TweetItem 
-            tweet={tweet.content}
-            onRemove={removeTweetHandler.bind(null, tweet)}
-            />
-        ))
+      let tweets = await response.json();
+      console.log("Timeline_tweets", tweets);
+      setTweetList(tweets);
+    }
+    fetchData();
+  }, [useEffectDependency]);
 
-    return (
-        <div className="App" >
-                <div className={classes.timeline}>
-                <h2>TimeLine</h2>
-               
-                <ul>{Tweets}</ul> 
-         
-                <Link to="/create-tweet">
-                   <button type="button" className={classes.button}> New Tweet </button> 
-                </Link>
+  //remove tweet handler
+  const removeTweetHandler = async (tweetItem) => {
+    setUseUffectDependency(tweetItem);
+    let url = `http://localhost:3005/tweets/${tweetItem._id}`;
+    let response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    });
+  };
 
-                </div>
-               
-        </div>
-    );
+  //update tweet handler
+  const updateTweetHandler = async (id, updatedTweet) => {
+    console.log("updateTweetHandler_id", id);
+    setUseUffectDependency(updatedTweet);
+    let url = `http://localhost:3005/tweets/${id}`;
+
+    let requestBody = {
+      content: updatedTweet,
+    };
+
+    let response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(requestBody),
+    });
+  };
+
+  const Tweets = tweetList.map((tweet) => (
+    <TweetItem
+      id={tweet._id}
+      tweet={tweet.content}
+      onRemove={removeTweetHandler.bind(null, tweet)}
+      updateTweetField={updateTweetHandler}
+    />
+  ));
+
+  return (
+    <div className="App">
+      <div className={classes.timeline}>
+        <h2>TimeLine</h2>
+        <ul>{Tweets}</ul>
+        <Link to="/create-tweet">
+          <button type="button" className={classes.button}>
+            New Tweet
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Timeline;
--- src/Tweets/TweetItem.js
+++ src/Tweets/TweetItem.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React from "react";
 import classes from "./Tweet.module.css";
+import { useState, useRef } from "react";
 
 function TweetItem(props) {
-    return (
-        <div className={classes.tweet}>
-           
-            <span >{props.tweet}</span>
-            <button onClick={props.onRemove} className={classes.delete}>Delete</button>
-        </div>
-    );
+  const [isEdit, setIsEdit] = useState(false);
+  const [updatedTweet, setUpdatedTweet] = useState(props.tweet);
+  const tweetInputRef = useRef();
+
+  const setCancelHandler = (event) => {
+    setUpdatedTweet("");
+    setIsEdit(!isEdit);
+  };
+
+  const setFieldEditable = (event) => {
+    setIsEdit(!isEdit);
+  };
+
+  const updateTweet = (event) => {
+    setUpdatedTweet(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    setIsEdit(!isEdit);
+    const tweet = tweetInputRef.current.value;
+    props.updateTweetField(props.id, tweet);
+  };
+
+  return (
+    <div className={classes.tweet}>
+      {!isEdit && <span>{props.tweet}</span>}
+      {!isEdit && (
+        <button onClick={setFieldEditable} className={classes.edit}>
+          Edit
+        </button>
+      )}
+      {isEdit && (
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={updatedTweet}
+            onChange={updateTweet}
+            name="updateTweet"
+            ref={tweetInputRef}
+          />
+          <button type="submit" className={classes.edit}>
+            Ok
+          </button>
+          <button
+            type="button"
+            onClick={setCancelHandler}
+            className={classes.edit}
+          >
+            Cancel
+          </button>
+        </form>
+      )}
+      {!isEdit && (
+        <button onClick={props.onRemove} className={classes.delete}>
+          Delete
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default TweetItem;
